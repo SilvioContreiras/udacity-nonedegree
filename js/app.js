f77b527b2be6f04148d281c7abc7a29c33b36f52
SilvioContreiras <silvio7dcontreiras@gmail.com>
2018-08-16 18:49:39 -0300

    Add some style and js funtionalities

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 const myCards =  document.querySelectorAll('.my-card');
 let myScore =  document.getElementById('score').textContent = 0;
 
+let card =  document.querySelectorAll('.front');
+
 
 let changeCard = false;
+let block = false;
 let cardOne, cardTwo;
-let close = false;
 
 function showCard() {
-	if (close) return;
+	if (block) return;
 
 	if(this === cardOne) return;
 	this.classList.add('show');
 
 	if (!changeCard) {
+
+		// First Click 
+
 		changeCard = true;
 		cardOne = this;
 
@@ -22,27 +27,45 @@ function showCard() {
 		changeCard = false;
 		cardTwo = this;
 
+		//  Check if the cards matches or not
 	if (cardOne.dataset.name === cardTwo.dataset.name) {
+
+		//  remove the function that make the card to flip
 		cardOne.removeEventListener('click', showCard);
 		cardTwo.removeEventListener('click', showCard);
 
-			
+		setTimeout(function(){
 
+			for (let i = 0; i < card.length; i++) {
+				card[i].classList.add('match');
+
+				console.log('Awesome');
+			}
+
+		}, 200);
+
+		resetGame();
 
 	} else {
 
 		let countPlay = myScore + 1; 
 		myScore = document.getElementById('score').innerHTML = countPlay;
 
-		close = true;
-
+		block = true;
 		setTimeout(function(){
 			cardOne.classList.remove('show');
 			cardTwo.classList.remove('show');
 
-			close =  false;
-		}, 1500);
+			block =  false;
+		}, 600);		
 	} 
+}
+
+function resetGame() {
+	changeCard = false;
+	block = false;
+	cardOne = null;
+	cardTwo = null;
 }
 
 (function shuffle() {
@@ -53,14 +76,14 @@ function showCard() {
 })();
 
 
-function resetGame() {
-	changeCard = false;
-	close = false;
-	cardOne = null;
-	cardTwo = null;
-}
-
 
 myCards.forEach(function(card){
 	card.addEventListener('click', showCard);
 });
+
+
+
+
+
+
+
