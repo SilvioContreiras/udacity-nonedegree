0ea5311b5b29c760a12822dfe341fc48fc31f7c3
SilvioContreiras <silvio7dcontreiras@gmail.com>
2018-08-20 11:43:24 -0300

    Add new functions to the js file

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
 const myCards =  document.querySelectorAll('.my-card');
-let myScore =  document.getElementById('score').textContent = 0;
+let numMove =  document.getElementById('moveCount').textContent = 0;
 
-
-let timer =  '';
+let countMatch = 0;
 let timerDelay = 0;
 let seg = 0;
 let min = 0;
-let timerCount =  document.querySelector('.timer');
+let timerCount =  document.querySelector('.time');
+let time =  '';
+let timeResting = 0;
 let reiniciar = document.querySelector('.reInit');
 let modal = document.querySelector('.modal');
 let felicitacoes =  document.querySelector('.congratulations');
+let textTime = '';
+let modalMensag = ''; 
 let status =  document.querySelector('.statistcs');
+let estrelas = document.querySelector('.all-stars').getElementByTagName('li');
+let contStars =  3;
 
+reiniciar.addEventListener('click', reinInitGame);
 
-// reiniciar.addEventListener('click', reinInitGame);
-
-// let card =  document.querySelector('.front');
 
 
 let changeCard = false;
@@ -31,7 +34,6 @@ function showCard() {
 	if (!changeCard) {
 
 		// First Click 
-
 		changeCard = true;
 		cardOne = this;
 
@@ -44,7 +46,11 @@ function showCard() {
 		//  Check if the cards matches or not
 	if (cardOne.dataset.name === cardTwo.dataset.name) {
 		checarMatch();
-	}
+
+		countMatch ++;
+		youWin();
+
+	} 
 
 	else {
 
@@ -72,8 +78,8 @@ function resetGame() {
 // Couts the number of steps failed.
 
 function numberSteps() {
-	let countPlay = myScore + 1; 
-	myScore = document.getElementById('score').innerHTML = countPlay;
+	let countPlay = numMove + 1; 
+	numMove = document.getElementById('moveCount').innerHTML = countPlay;
 }
 
 
@@ -81,7 +87,6 @@ function checarMatch() {
 	//  remove the function that make the card to flip
 	flipCards();
 	resetGame();
-
 }
 
 // When there is no match removes the flipped card
@@ -95,13 +100,65 @@ function noMatch() {
 }
 
 //  Flips the card to front 
-
 function flipCards() {
 	cardOne.removeEventListener('click', showCard);
 	cardTwo.removeEventListener('click', showCard);
 
 	cardOne.classList.add('shake');
 	cardTwo.classList.add('shake');
+}
+
+// Game start
+// function initGame() {
+// 	for (let i = 0; i < myCards.length; i++) {
+// 		myCards[i].addEventListener(click, showCard)
+// 	}
+// }
+
+
+// Init the the time of the game
+function timeStart() {
+	time = setInterval(buildTime, 1000);
+}
+
+
+// Calculate minutes and seconds 
+function buildTime() {
+	sec += 1;
+	min =  Math.floor(sec / 60);
+	sec = Math.floor(sec % 60);
+	textTime = pad(min) + ":" + pad(sec);
+	timerCount.innerHTML =  textTime;
+} 
+
+
+function pad(value) {
+	let txt = value + "";
+	if(txt.length < 2){
+		return "0" + txt;
+	} else {
+		return txt;
+	}
+}
+
+function stopTime() {
+	clearInterval(time);
+	sec = 0;
+	min = 0;
+	textTime = '';
+}
+
+function createsMensg() {
+	modalMensag = "<p>You made" + numMove + "moves in" + min + "minutes" + sec + "seconds!</p>" + "<p>you received" + contStars + "star</p>"
+}
+
+
+// Verify if you win the game.
+function youWin() {
+
+	if (countMatch === 8) {
+		console.log("You Win");
+	}
 }
 
 
